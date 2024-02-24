ede8508ef745ab461af8a8ddb02f57d428b35a77
zxdanz <87614060+zxdanz@users.noreply.github.com>
2024-02-23 16:34:24 -0800

    two more images of cats, the button press changes pic1 to pic2 yeyyyyy

--- very epic app/ViewController.swift
+++ very epic app/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pic1: UIImageView!
+    @IBOutlet weak var pic2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        pic1.image = UIImage(named : "pic2")
+        pic2.isHidden = false;
+    }
+    
 }
